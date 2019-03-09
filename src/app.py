f1a74ddcb0e256ffb0716cbe99e035fc40ccb3f0
Michal <m.mwojcik@outlook.com>
2019-03-09 14:03:35 +0100

    hotfix docker-compose ports -> parse to int

--- src/app.py
+++ src/app.py
@@ -11,5 +11,5 @@ app = configure_error_handlers(app=app)
 app.add_api('timers_api_documentation.yaml', strict_validation=True)
 
 if __name__ == '__main__':
-    connect(environ['MONGO_DATABASE_NAME'], host=environ['MONGO_DATABASE_SERVER'], port=environ['MONGO_DATABASE_PORT'])
-    app.run(port=environ['FLASK_DEFAULT_PORT'], debug=app_is_debug_mode, use_reloader=app_use_reloader, threaded=False)
+    connect(environ['MONGO_DATABASE_NAME'], host=environ['MONGO_DATABASE_SERVER'], port=int(environ['MONGO_DATABASE_PORT']))
+    app.run(port=int(environ['FLASK_DEFAULT_PORT']), debug=app_is_debug_mode, use_reloader=app_use_reloader, threaded=False)
